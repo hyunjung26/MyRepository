a7ae461d3a8f0181f40ee98a66094d850f544022
USER <USER@DESKTOP-IBVPFDO>
2023-12-23 21:06:55 +0900

    First commit in devB branch

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -4,6 +4,8 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version.");
+		System.out.println("A");
+		System.out.println("master");
 		}
 }
 
