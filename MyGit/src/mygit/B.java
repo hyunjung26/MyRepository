fd162386e9f70c80dc7d0010b65c9e8b63e6b9bc
USER <USER@DESKTOP-IBVPFDO>
2023-12-23 20:50:27 +0900

    Initial version

--- MyGit/src/mygit/B.java
+++ MyGit/src/mygit/B.java
@@ -1,5 +1,8 @@
 package mygit;
 
 public class B {
-
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+		System.out.println("This is the 2nd version.");
+		}
 }
--- MyGit/src/mygit/C.java
+++ MyGit/src/mygit/C.java
@@ -1,5 +1,8 @@
 package mygit;
 
 public class C {
-
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+		System.out.println("This is the 2nd version.");
+		}
 }
